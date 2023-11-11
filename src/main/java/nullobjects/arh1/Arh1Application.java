bc1e3a90738838fa9af6b39a551a45f3c5cb3ec4
Bhoonn <62685365+Bhoonn@users.noreply.github.com>
2023-11-11 16:18:27 +0100

    feat: push java spring project

---
+++ src/main/java/nullobjects/arh1/Arh1Application.java
@@ -0,0 +1,13 @@
+package nullobjects.arh1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Arh1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Arh1Application.class, args);
+	}
+
+}
---
+++ src/test/java/nullobjects/arh1/Arh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package nullobjects.arh1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Arh1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
