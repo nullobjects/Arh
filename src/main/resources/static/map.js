3418c7a407db01dae1f294fcfeaa42237af82a21
viks <viks3@protonmail.com>
2023-11-11 21:25:05 +0100

    marker2

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -8,14 +8,12 @@ let mapa = new L.map('map', options);
 let layer = new L.TileLayer('https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=2a2a8fb8338646f1bfecaefa5e7de596');
 mapa.addLayer(layer);
 
-let marker1 = new L.marker([41.607,21.753], {
-    color: 'red',
-    fillColor: 'FF0033',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mapa)
-
 mapa.setMaxBounds([
     [42.472,24.082],
-    [40.647,19.841],  
+    [40.647,19.841],
 ]);
+
+let marker1 = new L.marker([41.607,21.753], {
+}).addTo(mapa)
+
+let arta = new L.marker([42.00340266659381, 21.391197279274454], {}).addTo(mapa)
