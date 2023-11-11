c9f2705cc97ba312625f092344fa820323ab9bf5
viks <viks3@protonmail.com>
2023-11-11 21:16:35 +0100

    map v2

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,16 +1,21 @@
 let options = {
     center:[41.607, 21.753],
-    zoom:9
+    zoom:9,
+    minZoom:9
 }
 
 let mapa = new L.map('map', options);
-let layer = new L.TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png');
+let layer = new L.TileLayer('https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=2a2a8fb8338646f1bfecaefa5e7de596');
 mapa.addLayer(layer);
 
-//marker
-let marker1 = new L.marker([51,21], {
+let marker1 = new L.marker([41.607,21.753], {
     color: 'red',
-    fillColor: '#f03',
+    fillColor: 'FF0033',
     fillOpacity: 0.5,
     radius: 500
 }).addTo(mapa)
+
+mapa.setMaxBounds([
+    [42.472,24.082],
+    [40.647,19.841],  
+]);
