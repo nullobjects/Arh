1db5af70961fc3b90c78b666a83f892816dbc373
Mir028 <95441042+Mir028@users.noreply.github.com>
2023-11-11 19:59:22 +0100

    Updatemap

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -8,3 +8,9 @@ let layer = new L.TileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png');
 mapa.addLayer(layer);
 
 //marker
+let marker1 = new L.marker([51,21], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mapa)
